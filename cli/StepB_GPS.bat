4a099449f36c26b00bd783dd2dd013ad8cf4be46
qz <zhaoqige@163.com>
2017-10-13 17:09:50 +0800

    Update .BAT files (verified user choice)

--- cli/StepB_GPS.bat
+++ cli/StepB_GPS.bat
@@ -1,10 +1,24 @@
 @echo off
 title "ARNPerf v7.0.121017-py"
 set DIR=%cd%
+set /p conf="Search automatically: Y/n ? "
+
+if "%conf%" == "" goto arnperf7search
+if "%conf%" == "y" goto arnperf7search
+if "%conf%" == "Y" goto arnperf7search
+
+:arnperf7user
 set /p num="Enter GPS Sensor: (com4) com"
-if "%num%" == "" (
-	%DIR%\GPS.py
-) else (
-	%DIR%\GPS.py com"%num%"
-)
+if "%num%" == "" set num=4
+%DIR%\GPS.py com"%num%"
+
 pause
+exit
+
+:arnperf7search
+echo * ARNPerf GPS search: enabled
+%DIR%\GPS.py
+
+echo
+pause
+exit
--- cli/StepC_ARNPerf.bat
+++ cli/StepC_ARNPerf.bat
@@ -2,20 +2,30 @@
 title "ARNPerf v7.0.121017-py"
 set DIR=%cd%
 set /p conf="Use ARNPerf.conf: Y/n ? "
-if ("%conf%" == "" OR "%conf%" == "y" OR "%conf%" == "Y") (
-	%DIR%\Perf.py
-	pause
-	exit
-) else (
-	set /p hid="Enter Host IP: 192.168.1."
-	if "%hid%" == "" set hid="24"
-	set /p log="Enter Log Filename: (d24fast.log) "
-	if "%log%" == "" set log="d24fast.log"
-	set /p note="Enter Note: (demo) "
-	if "%note%" == "" set note="demo"
-	set /p location="Enter Location: (BQL) "
-	if "%location%" == "" set location="BQL"
-	%DIR%\Perf.py 192.168.1."%host%" "%log%" "%note%" "%location%"
-	pause
-	exit
-)
+
+if "%conf%" == "" goto arnperf7conf
+if "%conf%" == "y" goto arnperf7conf
+if "%conf%" == "Y" goto arnperf7conf
+
+:arnperf7user
+set /p hid="Enter Host IP: 192.168.1."
+if "%hid%" == "" set hid="24"
+set /p log="Enter Log Filename: (d24fast.log) "
+if "%log%" == "" set log="d24fast.log"
+set /p note="Enter Note: (demo) "
+if "%note%" == "" set note="demo"
+set /p location="Enter Location: (BQL) "
+if "%location%" == "" set location="BQL"
+%DIR%\Perf.py 192.168.1."%host%" "%log%" "%note%" "%location%"
+
+echo
+pause
+exit
+
+:arnperf7conf
+echo * using ARNPerf.conf
+%DIR%\Perf.py
+
+echo
+pause
+exit
